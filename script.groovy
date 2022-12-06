69043e321c52124f5cbbf9c07a52628546b58178
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2022-12-06 20:32:33 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -18,7 +18,7 @@ def sonarScan() {
         echo "Running sonarQube scan..."
         withSonarQubeEnv('sonarqube') {
            // sh "${scannerHome}/bin/sonar-scanner"
-              sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=sonarqube"
+              sh "mvn clean verify sonar:sonar -Dsonar.projectKey=sonarqube"
         }
         timeout(time: 10, unit: 'MINUTES') {
             waitForQualityGate abortPipeline: true
