54385d2dd8ad8ce34383683c1c5f7f81b8eb43f4
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2022-12-06 19:19:39 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sofienechihi/my-repo:spring-app-1.0 .'
+        sh 'docker build -t yassine15/demo-app:spring-app-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push sofienechihi/my-repo:spring-app-1.0'
+        sh 'docker push yassine15/demo-app:spring-app-1.0'
     }
 }
 
